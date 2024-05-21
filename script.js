25c6a16d1b67953f9593ee2380ca1b575b29d75b
Rachel Pizane <rachel.pzm@gmail.com>
2024-05-21 15:36:35 -0300

    Iniciando um repositorio

---
+++ script.js
@@ -0,0 +1,109 @@
+// ---- Global ----
+let regioes = [
+  "África",
+  "América do Norte",
+  "América do Sul",
+  "Antártida",
+  "Ásia",
+  "Europa",
+  "Oceania",
+];
+
+
+let paises = [];
+
+let paginaAtual = 1;
+let qntdPorPagina = 3
+
+
+// ---- Front ----
+
+function selecaoRegiao(regioes,id) {
+  let select = document.getElementById(id);
+
+  regioes.forEach((func) => {
+    let option = document.createElement("option");
+    option.innerText = func;
+    select.appendChild(option);
+  });
+}
+function incluir(){
+    let nome = document.getElementById("nome").value;
+    let regiao = document.getElementById("regiao").value;
+
+    inclusao(nome, regiao)
+    atualizarDados()
+}
+
+function atualizarDados(paisesFilter = null){
+
+    let paisesAtualizado = paisesFilter == null ? paises : paisesFilter;
+
+    let tabela = document.getElementById("tabela");
+
+    tabela.innerHTML =""
+    paisesAtualizado.forEach((pais)=>{
+        let newLine = document.createElement("tr")
+        for(j in pais){
+            let newData= document.createElement("td")
+            newData.innerHTML = pais[j];
+            newLine.appendChild(newData)
+        }
+        tabela.appendChild(newLine)
+    })
+}
+
+function filtrarRegiao(){
+    let regiao = document.getElementById("filtroRegioes").value
+    if(regiao === ""){
+        atualizarDados()
+    } else{
+        let paisesFilter = paises.filter((pais)=>{
+            return regiao === pais.regiao
+        });
+        atualizarDados(paisesFilter) 
+    }
+}
+
+function proximo(){
+    let totalPag = Math.ceil(paises.length / qntdPorPagina);
+    if(paginaAtual < totalPag){
+        let a = (paginaAtual - 1) * qntdPorPagina;
+        let b = a + qntdPorPagina
+        atualizarDados().slice(a, b)
+        paginaAtual++
+        console.log(paginaAtual)
+    } else{
+        alert("Fim")
+    }
+}
+// Chamadas ----
+selecaoRegiao(regioes, "regiao");
+selecaoRegiao(regioes, "filtroRegioes");
+
+//   ---- Back ----
+function inclusao(nome, regiao){
+    class pais{
+        constructor(nome, regiao){
+            this.nome = nome;
+            this.regiao = regiao;
+        }
+    }
+    paises.push(new pais(nome, regiao))
+}
+
+let num = [1,2,5,6,4,7,8,9,10,13]
+let totalPagNum = Math.ceil(num.length / 3);
+let a = 4;
+let b = a + 3;
+console.log(num.slice(a, b))
+console.log(totalPag)
+
+for(let i = 0; i < totalPagNum; i++){
+    let a = i * qntdPorPagina;
+    let b = a + qntdPorPagina;
+    console.log(a)
+    console.log(num.slice(a, b))
+}
+
+
